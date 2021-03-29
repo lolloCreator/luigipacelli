8dfdc95881f795c5034564817a2db485ca1535fa
lolloCreator <luigipacelli27@gmail.com>
2021-03-29 23:09:04 +0200

    primo vs commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -659,7 +659,7 @@
 <!-- Licensing information: https://bootstrapmade.com/license/ -->
 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/personal-free-resume-bootstrap-template/ -->
 
-Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+Designed by <a href="https://bootstrapmade.com/">lolloCreator</a>
 
 
 
