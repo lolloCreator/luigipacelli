2f88482c576fd1a402e6f398703307051a119214
lolloCreator <luigipacelli27@gmail.com>
2021-04-06 20:09:58 +0200

    favicon+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
   <meta content="HTML, CSS, JavaScript" name="keywords">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
+  <link href="{{ setting('site.logo') }}" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 
